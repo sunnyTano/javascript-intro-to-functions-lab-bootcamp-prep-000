30bdd579163822cfdbec2b401dc77c340292e100
Susan Tan <susan.tan198@gmail.com>
2019-01-28 21:13:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(str){
   if(str === str.toLowerCase()){
     return "I can\'t hear you!"
   } else if(str === str.toUpperCase()){
-    return "YES INDEED"
+    return "YES INDEED!"
   } else if(str === "I love you, Grandma"){
     return "I love you, too"
   }
