340c9526b03002ad67796847195a1788eb78582a
Susan Tan <susan.tan198@gmail.com>
2019-01-28 21:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function sayHiToGrandma(str){
 }
 
 describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+  it('returns "I can\t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
 
