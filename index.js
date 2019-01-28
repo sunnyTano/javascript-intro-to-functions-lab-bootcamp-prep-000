01fce6e6b826b87d6f4649b8e6ba02f9697f1812
Susan Tan <susan.tan198@gmail.com>
2019-01-28 21:10:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ function logWhisper(str){
   console.log(str.toLowerCase());
 }
 
+function sayHiToGrandma(str){
+  if(str === str.toLowerCase()){
+    return "I can\t hear you!"
+  } else if(str === str.toUpperCase()){
+    return "YES INDEED"
+  } else if(str === "I love you, Grandma"){
+    return "I love you, too"
+  }
+}
+
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
